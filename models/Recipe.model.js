1d21ad54409d87ead9db8cac5cb820dc23d4ad2e
Simone Guimaraes <simoneguimaraes11@gmail.com>
2021-12-02 17:54:23 -0300

    initial commit

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,10 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"],
+  },
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
+const Recipe = mongoose.model("Recipe", recipeSchema);
 
 module.exports = Recipe;
